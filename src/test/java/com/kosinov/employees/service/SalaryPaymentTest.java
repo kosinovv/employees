69bb6bbd291dfb92fa3287f26a562350286af30e
kosinovv <kosinov_va@mail.ru>
2024-06-10 20:19:48 +0300

    Доработка юнит-тестов.

---
+++ src/test/java/com/kosinov/employees/service/SalaryPaymentTest.java
@@ -0,0 +1,65 @@
+package com.kosinov.employees.service;
+
+import com.kosinov.employees.dto.SalaryPaymentDTO;
+import com.kosinov.employees.mapper.SalaryMapperImpl;
+import com.kosinov.employees.repository.SalariesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        SalaryPaymentService.class,
+        SalaryMapperImpl.class})
+
+public class SalaryPaymentTest {
+
+    @Autowired
+    private SalaryPaymentService salaryPaymentService;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @MockBean
+    private SalariesRepository salariesRepository;
+
+    private SalaryPaymentDTO salaryPaymentDTO;
+
+
+    @BeforeEach
+    public void setupSalaryPayment() throws ParseException {
+        salaryPaymentDTO = new SalaryPaymentDTO();
+        salaryPaymentDTO.setEmployeeTabNum("1");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date salaryDate = sdf.parse("2024-06-10");
+        salaryPaymentDTO.setPaymentdate(salaryDate);
+        salaryPaymentDTO.setSalarySum(2000.0);
+    }
+
+    @Test
+    void createSalaryPayment_test() {
+        //Проверка работы метода добавления
+        salaryPaymentDTO = salaryPaymentService.createSalary(salaryPaymentDTO);
+    }
+
+    @Test
+    void findSalaryPayment_test() {
+        //Проверка работы метода поиска
+        salaryPaymentDTO = salaryPaymentService.findSalary(1);
+    }
+
+    @Test
+    void deleleSalaryPayment_test() {
+        //Проверка работы метода поиска
+        salaryPaymentDTO = salaryPaymentService.deleteSalary(1);
+    }
+}
